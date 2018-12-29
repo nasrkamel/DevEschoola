a25055b7846ca1ab1d2817b49cb261353f9ef6f7
nasrkamel <nasrkamel2012@gmail.com>
2018-12-29 23:00:53 +0200

    slider comment finale

--- js/Custom_JS.js
+++ js/Custom_JS.js
@@ -2,18 +2,14 @@
 $(window).scroll(function () {
 
     var x = $(window).scrollTop();
-    if (x > 300) {
-        $("#btnUp").fadeIn(1000);
+    if (x > 10) {
+ 
         $("#NavBar").addClass("coloor");
     }
     else {
-        $("#btnUp").fadeOut(1000);
+ 
         $("#NavBar").removeClass("coloor");
     }
 })
 
-$('#btnUp').click(function () {
-   
-    $("body").animate({ scrollTop: 0 }, 1000)
-
-})
+ 
